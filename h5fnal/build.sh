c361bd7e21204351b189d1c8c088c213b0e56093
Dana Robinson <derobins@hdfgroup.org>
2017-06-01 06:57:18 -0400

    Updated the build script to use a variable for the location of HDF5.

--- h5fnal/build.sh
+++ h5fnal/build.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-
+# Path to HDF5 installation bin directory
+HDF5=../../hdf5_d/bin/
 
 # libh5fnal
-../../hdf5/bin/h5cc -c h5fnal.c -o h5fnal.o
+${HDF5}h5cc -c h5fnal.c -o h5fnal.o
 ar rcs libh5fnal.a h5fnal.o
 
 
 # Vector of MC Hit Collection
-../../hdf5/bin/h5cc -lh5fnal -L. -o test_v_mc_hit_collection test_v_mc_hit_collection.c
+${HDF5}h5cc -lh5fnal -L. -o test_v_mc_hit_collection test_v_mc_hit_collection.c
 ./test_v_mc_hit_collection
-../../hdf5/bin/h5ls -r v_mc_hc.h5
-#../hdf5/bin/h5dump -H v_mc_hc.h5
-#../hdf5/bin/h5dump --dataset="testrun/testevent/vomchc" -c 3 v_mc_hc.h5
+${HDF5}h5ls -r v_mc_hc.h5
+#${HDF5}h5dump -H v_mc_hc.h5
+#${HDF5}h5dump --dataset="testrun/testevent/vomchc" -c 3 v_mc_hc.h5
 
 
 # Vector of MC Truth
-../../hdf5/bin/h5cc -lh5fnal -L. -o test_v_mc_truth test_v_mc_truth.c
+${HDF5}h5cc -lh5fnal -L. -o test_v_mc_truth test_v_mc_truth.c
 ./test_v_mc_truth
-../../hdf5/bin/h5ls -r v_mc_truth.h5
+${HDF5}h5ls -r v_mc_truth.h5
