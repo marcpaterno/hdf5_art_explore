9f2a707f9e44b334eccdd83c87834b57d009816a
Marc Paterno <paterno@fnal.gov>
2017-06-21 15:49:40 -0500

    Prepare for speed measurement of reading

--- art_demo/art_demo.cc
+++ art_demo/art_demo.cc
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,46 +11,50 @@
 #include "lardataobj/RecoBase/Vertex.h"
 #include "nusimdata/SimulationBase/MCTruth.h"
 
-
 using namespace art;
 using namespace std;
+using namespace std::chrono;
+using namespace std::string_literals;
 
-int main() {
+int main(int argc, char** argv) {
+  // When this gets updated to also read an HDF5 file, we should
+  // probably be set up to always expect argc == 3, and thus two
+  // filenames.
+  if (argc == 1) {
+    cout << "Please specify the name of one or more art/ROOT input file(s) to "
+      "read.\n";
+    return 1;
+  }
 
   InputTag mctruths_tag { "generator" };
-  InputTag vertex_tag { "linecluster" };
-  InputTag assns_tag  { "linecluster" };
-  vector<string> filenames { "../data/dune.root" }; // multiple files are allowed.
-
+  // This reads all the command-line arguments into a vector of
+  // strings. We don't care about argv[0], the program name.
+  vector<string> filenames(argv + 1, argv + argc);
 
   // The gallery::Event object acts as a cursor into the stream of events.
   // A newly-constructed gallery::Event is at the start if its stream.
   // Use gallery::Event::atEnd() to check if you've reached the end of the stream.
   // Use gallery::Event::next() to go to the next event.
 
+  vector<microseconds> root_times; // times for reading ROOT
+  vector<microseconds> hdf_times;  // times for reading HDF5
+
   for (gallery::Event ev(filenames); !ev.atEnd(); ev.next()) {
-    // getValidHandle() is preferred to getByLabel(), for both art and
-    // gallery use. It does not require in-your-face error handling.
-    std::vector<simb::MCTruth> const& mctruths = *ev.getValidHandle<vector<simb::MCTruth>>(mctruths_tag);
+    // Do moving on to the next event in the HDF5 file here, before
+    // the first timing call.
+    auto const t0 = system_clock::now();
+    auto  const&  mctruths [[gnu::unused]] =
+      *ev.getValidHandle<vector<simb::MCTruth>>(mctruths_tag);
+    root_times.push_back(duration_cast<microseconds>(system_clock::now()-t0));
+    auto const t1 = system_clock::now();
+    // do HDF reading here.
+    hdf_times.push_back(duration_cast<microseconds>(system_clock::now()-t1));
+  }
 
-    // In C++11 or newer, we recommend use of 'auto' to deduce the
-    // type of the value of the expression (here, the return value of
-    // the function call and derefencing of the returned ValidHandle<...>
-    // object).
-    //
-    //auto const& mctruths = *ev.getValidHandle<vector<simb::MCTruth>>(mctruths_tag);
-
-    // After the call above, the MCTruth objects have been read into
-    // memory, and any of their *const* functions can be invoked. Only
-    // const functions can be used, because we have a const reference
-    // to the objects. This is part of the design of our object model:
-    // objects *read from an Event* are always immutable.
-
-    if (!mctruths.empty()) {
-      // This output will not be very interesting if the file you're
-      // looking at is the kind of simulation that only contains one
-      // particle per MCTruth object.
-      std::cout << mctruths[0].NParticles() << '\n';
-    }
+  // Write out the times to a standard output, in a way easily
+  // readable with R (or many other tools).
+  cout << "root\thdf5\n";
+  for (size_t i = 0, sz = root_times.size(); i != sz; ++i) {
+    cout << root_times[i].count() << '\t' << hdf_times[i].count() << '\n';
   }
 }
