8ed9b2d5ead8655673fe3e3ff0a384a22e8a400b
Dana Robinson <derobins@hdfgroup.org>
2017-08-14 11:19:51 -0700

    Added string dictionary empty files.

---
+++ h5fnal/src/string_dictionary.c
@@ -0,0 +1,8 @@
+/* string_dictionary.c */
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "h5fnal.h"
+#include "string_dictionary.h"
+
---
+++ h5fnal/src/string_dictionary.h
@@ -0,0 +1,18 @@
+/* string_dictionary.h
+ *
+ * Header for an internal string dictionary.
+ */
+
+#ifndef H5FNAL_STRING_DICTIONARY_H
+#define H5FNAL_STRING_DICTIONARY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* H5FNAL_STRING_DICTIONARY_H */
+
