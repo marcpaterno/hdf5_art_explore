f4b6603bd7185a9d78848c1a6edbebd0e90154b0
Dana Robinson <derobins@hdfgroup.org>
2017-08-14 20:34:46 -0700

    Added stubs and a type for the string dictionary.

--- h5fnal/src/string_dictionary.c
+++ h5fnal/src/string_dictionary.c
@@ -6,3 +6,28 @@
 #include "h5fnal.h"
 #include "string_dictionary.h"
 
+herr_t
+create_string_dict(string_dictionary_t *dict)
+{
+    return H5FNAL_FAILURE;
+}
+
+herr_t
+destroy_string_dict(string_dictionary_t *dict)
+{
+    return H5FNAL_FAILURE;
+}
+
+unsigned
+add_string_to_dict(const char *s)
+{
+    return 0;
+}
+
+unsigned
+get_string_index(const char *s)
+{
+    return 0;
+}
+
+
--- h5fnal/src/string_dictionary.h
+++ h5fnal/src/string_dictionary.h
@@ -6,9 +6,20 @@
 #ifndef H5FNAL_STRING_DICTIONARY_H
 #define H5FNAL_STRING_DICTIONARY_H
 
+
+typedef struct string_dictionary_t {
+    unsigned n_strings;
+    char **strings;
+} string_dictionary_t;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+herr_t create_string_dict(string_dictionary_t *dict);
+herr_t destroy_string_dict(string_dictionary_t *dict);
+unsigned add_string_to_dict(const char *s);
+unsigned get_string_index(const char *s);
 
 #ifdef __cplusplus
 }
