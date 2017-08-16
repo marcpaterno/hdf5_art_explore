d5aaaadffb9102d645febee9852070d55f3ba153
Marc Paterno <paterno@fnal.gov>
2017-08-16 13:45:25 -0500

    Expect HDF5 tools to be on PATH

--- h5fnal/test/test_h5fnal.sh
+++ h5fnal/test/test_h5fnal.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-HDF5_BINDIR="../../../hdf5/bin"
-
 # Get rid of old files
 rm -rf v_mc_hc.h5
 rm -rf test_v_mc_hit_collection.h5ls.out
@@ -15,7 +13,7 @@ rm -rf test_v_mc_hit_collection.h5dump_dataset.out
 
 # Check HDF5 tool output
 echo -n "Checking output: vector of MC Hit Collection (h5ls): "
-${HDF5_BINDIR}/h5ls -r v_mc_hc.h5 > test_v_mc_hit_collection.h5ls.out 2>&1
+h5ls -r v_mc_hc.h5 > test_v_mc_hit_collection.h5ls.out 2>&1
 diff test_v_mc_hit_collection.h5ls.correct test_v_mc_hit_collection.h5ls.out 2>&1 >/dev/null
 if [ $? -ne 0 ] ; then
     echo "*** FAILED ***"
@@ -24,7 +22,7 @@ else
 fi
 
 echo -n "Checking output: vector of MC Hit Collection (h5dump -H): "
-${HDF5_BINDIR}/h5dump -H v_mc_hc.h5 > test_v_mc_hit_collection.h5dump_H.out 2>&1
+h5dump -H v_mc_hc.h5 > test_v_mc_hit_collection.h5dump_H.out 2>&1
 diff test_v_mc_hit_collection.h5dump_H.correct test_v_mc_hit_collection.h5dump_H.out 2>&1 >/dev/null
 if [ $? -ne 0 ] ; then
     echo "*** FAILED ***"
